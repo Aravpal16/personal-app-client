c66210b4b4e9aab8d2e6c0e5c9db1fdb5fa0349f
Aravpal16 <36320357+Aravpal16@users.noreply.github.com>
2022-11-12 19:29:42 +0530

    Added header to the app

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # personal-app-client
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/personal-app-client)
---
+++ src/app/notes/notes.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'Notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.css'],
+})
+export class NotesComponent {}
